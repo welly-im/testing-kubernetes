50630bee285f223bb33a6d41d3ed6fec9125326d
Welly Immanuel <welly.im@i-saku.com>
2025-03-10 09:17:52 +0700

    add swagger

--- testing-kubernetes/Program.cs
+++ testing-kubernetes/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -13,6 +14,10 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
--- testing-kubernetes/obj/Debug/net9.0/testing-kubernetes.AssemblyInfo.cs
+++ testing-kubernetes/obj/Debug/net9.0/testing-kubernetes.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("e8ac73cc-3dd2-4d6e-9dec-de4188c7cdd3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("testing-kubernetes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5212a04403418b903585a824eb0f695ddfca159c")]
 [assembly: System.Reflection.AssemblyProductAttribute("testing-kubernetes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testing-kubernetes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
